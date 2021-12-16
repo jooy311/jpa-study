ad6656c150bcf8e038907497e327ea57df874cf2
jooy311 <jooy311@naver.com>
2021-12-16 22:21:48 +0900

    인프런 8강까지 학습한 내용 커밋

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,7 +5,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +19,8 @@ public class MemberRepositoryTest {
     @Autowired MemberRepository memberRepository;
 
     @Test
+    @Transactional
+    @Rollback(false)
     public void test() throws Exception{
         //given
         Member member = new Member();
@@ -28,5 +33,6 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 }
